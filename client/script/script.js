6fd494a4f3d335ec65ad1cbf52511016849c9f12
emieldecraemer <emiel.decraemer@student.tsaam.be>
2025-12-15 10:14:34 +0100

    indienen

--- client/script/script.js
+++ client/script/script.js
@@ -1,18 +1,23 @@
+// nodige variabelen ophalen
 const usernameInpt = document.querySelector("#username")
 const passwordInpt = document.querySelector("#password")
 const btnSignIn = document.querySelector("#btnSI")
 const sectionBefore = document.querySelector("#beforLogin")
+const sectionAfter = document.querySelector("#afterLogin")
 
+
+// object ophalen
 fetch("https://ex-kerst-2025.onrender.com/user/")
 .then(resultaat => resultaat.json())
 .then((gebruikers) => {
+    // functie geven aan je object
     console.log(gebruikers);
     gebruikers.forEach(gebruiker => {   // find() wou niet werken dus vervangen met forEach()
         if (gebruiker.login == usernameInpt.value && gebruiker.password == passwordInpt.value) {
             btnSignIn.addEventListener("click", () => {
-                console.log(usernameInpt.value);
+                sectionBefore.hidden = true
+                sectionAfter.hidden = false
             })
         }
     });
 })
-
